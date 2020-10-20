85f2192fe06bc1d52343a191b97ca125e091fb23
Mario Miličević <73180912+mariomilicevic2000@users.noreply.github.com>
2020-10-20 19:47:52 +0200

    Rename Source.c to 1lab_1zad.c

--- 1lab_1zad.c
+++ 1lab_1zad.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Napisati program koji prvo proËita koliko redaka ima datoteka, tj. koliko ima studenata
-zapisanih u datoteci. Nakon toga potrebno je dinamiËki alocirati prostor za niz struktura
-studenata (ime, prezime, bodovi) i uËitati iz datoteke sve zapise. Na ekran ispisati ime,
+/*Napisati program koji prvo pro√®ita koliko redaka ima datoteka, tj. koliko ima studenata
+zapisanih u datoteci. Nakon toga potrebno je dinami√®ki alocirati prostor za niz struktura
+studenata (ime, prezime, bodovi) i u√®itati iz datoteke sve zapise. Na ekran ispisati ime,
 prezime,apsolutni i relativni broj bodova.
-Napomena: Svaki redak datoteke sadrûi ime i prezime studenta, te broj bodova na kolokviju.
+Napomena: Svaki redak datoteke sadr≈æi ime i prezime studenta, te broj bodova na kolokviju.
 relatvan_br_bodova = br_bodova/max_br_bodova*100*/
 
 typedef struct studenti	//inicijalizacija strukture
